6e0d9b49fb5c2852de7552f02a788e4db8db867b
pawel.miera <pawel.miera@rad4m.com>
2024-11-25 23:28:43 +0100

    feat: update paths

--- src/services/Urls.tsx
+++ src/services/Urls.tsx
@@ -4,7 +4,7 @@ const development = 'http://localhost:8000/api';
 const production_front = 'https://mhosting.ipv64.net/groopie';
 const development_front = 'http://localhost:5173';
 
-const production_ws = 'wss://mhosting.ipv64.net/groopieApi/ws';
+const production_ws = 'ws://mhosting.ipv64.net/groopieApi/ws';
 const development_ws = 'ws://localhost:8000/api/ws/chat';
 
 export const ApiUrl = process.env.NODE_ENV === 'development' ? development : production;
